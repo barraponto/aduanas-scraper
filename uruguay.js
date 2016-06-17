e2afb0062a827b270865b4a9d361aa119aeb503e
Capi Etheriel <barraponto@gmail.com>
2016-06-17 20:18:13 -0300

    get the vprodcap options

---
+++ uruguay.js
@@ -0,0 +1,20 @@
+var casper = require('casper').create({
+  verbose: true,
+  loglevel: 'debug',
+  pageSettings: {
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
+  }
+});
+
+var initialURL = 'https://servicios.aduanas.gub.uy/LuciapubX/hpudbar2.aspx';
+var getOptions = function(){
+  var options = document.querySelectorAll('#vVPRODCAP option');
+  return Array.prototype.map.call(options, function(option){ return option.getAttribute('value'); });
+};
+
+casper.start(initialURL, function(){
+  var options = casper.evaluate(getOptions);
+  casper.echo(options);
+});
+
+casper.run();
