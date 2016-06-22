ddddb839cb4fab74190e5c99b623231a8268f3cf
Capi Etheriel <barraponto@gmail.com>
2016-06-22 01:57:35 -0300

    [tipos] imprime detalhes de navegação

--- 01-tipos.js
+++ 01-tipos.js
@@ -1,6 +1,6 @@
 var casper = require('casper').create({
   verbose: true,
-  loglevel: 'debug',
+  loglevel: 'info',
   pageSettings: {
     userAgent: 'Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0'
   }
